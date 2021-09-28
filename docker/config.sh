7c7a9fe2c0dca24f0b27f4a66d318313f22576cd
Zikang Xiong <zikangxiong@icloud.com>
2021-09-27 22:05:31 -0400

    fix bug and add comments

--- docker/config.sh
+++ docker/config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DOCKER_HUB_USERNAME=""
-PROJECT_NAME="ubuntu"
-DOCKER_TAG="latest"
+DOCKER_HUB_USERNAME="" # You docker hub user name
+PROJECT_NAME="ubuntu"  # Your project name on docker hub
+DOCKER_TAG="latest"    # Your project tag of your docker image
 
 if [ -z DOCKER_HUB_USERNAME ]
 then
-    BASE_IMAGE_NAME="$DOCKER_HUB_USERNAME/$PROJECT_NAME:$DOCKER_TAG"
-else
     BASE_IMAGE_NAME="$PROJECT_NAME:$DOCKER_TAG"
+else
+    BASE_IMAGE_NAME="$DOCKER_HUB_USERNAME/$PROJECT_NAME:$DOCKER_TAG"  
 fi
 
-WORKSPACE="/workspace"
-JUPYTER_PORT="8080"
-GPU="0"
+WORKSPACE="/workspace"  # The workspace name in docker
+JUPYTER_PORT="8080"     # The jupyter lab port exposed on the host
+GPU="0"                 # The number of gpu
